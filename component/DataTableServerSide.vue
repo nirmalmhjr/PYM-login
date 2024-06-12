a4580f5da5f4213374cd1c149f12ca1cced62bf8
nirmalmhjr <nirmalmhjr@gmail.com>
2024-06-12 16:57:24 +0545

    DataTableServerSide with interceptor

--- component/DataTableServerSide.vue
+++ component/DataTableServerSide.vue
@@ -8,21 +8,35 @@
       :search="search"
       @update:options="loadItems"
       item-value="name"
-    ></v-data-table-server>
+    >
+        <template v-slot:item.allow_delivery={item}>
+            <span>{{ item.allow_take_away ? 'available' :'not available' }}</span>
+        </template>
+        <template v-slot:item.allow_take_away={item}>
+            <span>{{ item.allow_take_away ? 'available' : 'not available' }}</span>
+        </template>
+    </v-data-table-server>
 </template>
 
 <script setup lang="ts">
+import {interceptor } from '~/composables/interceptor'
+import { useCheckTokenStore } from '~/store/checkToken';
+
+const tokenStore = useCheckTokenStore()
+const { userName } = tokenStore
+
+
 const props = defineProps({
-    userName: {
-        type: String,
+    headers:{
+        type:Array,
         required: true
-    },
-    datas:{
-        type:Object,
-        required: true
+        
     }
+    
+   
 })
 
+const headers= ref(props.headers)
 const itemsPerPage = ref(5)
 const search = ref('')
 const serverItems = ref([])
@@ -31,49 +45,28 @@ const totalItems = ref(0)
 
 
 
-const headers = ref([
+/* const headers = ref([
     { title: 'Name', value: 'name', align: 'start' },
     { title: 'Contact no.', value: 'contacts[0].contact_value' },
     { title: 'Take away', value: 'allow_take_away' },
     { title: 'Delivery', value: 'allow_delivery' },
     { title: 'Open at', value: 'opening_hours' },
     { title: 'closes at', value: 'closing_hours' },
-])
+]) */
 
 async function loadItems({ page, itemsPerPage }) {
     loading.value = true
      
     const offset = (page - 1) * itemsPerPage
-    // console.log(' load items function ran');
 
-/* 
-    let url = `https://pick.alldaycode.xyz/api/v1/users/${props.userName}/associated-orgs/?limit=${itemsPerPage}&offset=${offset}`
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${token}`
-        }
-    }
-
-    try {
-        const response = await fetch(url, config)
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        serverItems.value = data.results;
-        totalItems.value = data.count;
- */
 
     try{
-        console.log('data from data table server side', props.datas.results);
+        const response = await interceptor(`users/${userName}/associated-orgs/?limit=${itemsPerPage}&offset=${offset}`)
 
-        serverItems.value = props.datas.results
-        totalItems.value = props.datas.count
+
+        serverItems.value = response.results
+        totalItems.value = response.count
     } catch (error) {
         console.error('Fetch error:', error);
     } finally {
